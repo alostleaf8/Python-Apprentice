8b903dd09351344275e86b9a1474b4b9226e0bba
alostleaf8 <aishini.das@gmail.com>
2026-09-24 01:41:43 +0000

    Learning To Draw and Making Projects and Modulo (%)

--- lessons/10_Turtles/90_Graphics_Projects/10_Flaming_Ninja_Star.py
+++ lessons/10_Turtles/90_Graphics_Projects/10_Flaming_Ninja_Star.py
@@ -17,7 +17,7 @@ def get_random_color():
     return "#%06X" % (random.randint(0, 0xFFFFFF))
 
 
-colors = ["red", "blue", "green", "yellow", "orange"]
+colors = ["pink", "purple", "red", "lavender", "dark pink"]
 
 
 def get_next_color(i):
@@ -27,19 +27,19 @@ turtle.setup(600, 600, 0, 0)            # Set the size of the window
 window = turtle.Screen()
 
 base_size = 200  # the size of the black part of the star
-flame_size = 130  # the length of the flaming arms
+flame_size = 100  # the length of the flaming arms
 
 t = turtle.Turtle()
 t.shape("turtle")
 t.width(2)
 t.speed(0)
 
-for i in range(25):
+for i in range(20):
     t.pencolor(get_random_color())
     t.fillcolor(get_random_color())
     t.begin_fill()
     t.forward(64)
-    t.left(40)
+    t.right(40)
     t.forward(flame_size)
     t.right(170)
     t.forward(flame_size)
--- lessons/10_Turtles/90_Graphics_Projects/20_Crazy_Spiral.py
+++ lessons/10_Turtles/90_Graphics_Projects/20_Crazy_Spiral.py
@@ -7,26 +7,72 @@ in 10_Flaming_Ninja_Star.py, but use what you've learned about loops
 uid: zfzMbyH7
 name: Crazy Spiral
 """
+import turtle 
 
-... # Copy code to make a turtle and set up the window
+turtle.setup(600, 600, 0, 0)            # Set the size of the window
+window = turtle.Screen()
 
-t = ... # Create a turtle named t
+# Copy code to make a turtle and set up the window
 
-# 1) Complete make_a_shape() to make the turtle move in some pattern. 
+tina = turtle.Turtle()      # Create a turtle named tina
+tina.shape("circle")        # Make the shape of tina a turtle 
+tina.width(5)               # Make the width of tina 5
+tina.shapesize(3,3,1)      # Make the size of the cursor bigger 
+tina.speed(0)               # Make the speed of the tina that is moving 
+
+# 2) Complete make_a_shape() to make the turtle move in some pattern. 
 # For instance, you can make it go left 30 degrees, then forward 50 pixels, 
-# then right 60 degrees, then forward 100 pixels. Make any shape you like.
+# then right 60 degrees, then forward 100 pixels. Make any shape you like
 
+def make_a_shape2(t):
+    """Make a shape with turtle t. Make it go left or right or forward"""    
+    f=100
+    t.pencolor("#C50303")
+    t.begin_fill()
+    t.forward(f) 
+    t.right(130) 
+    t.forward(f) 
+    t.left(70) 
+    t.forward(f)
+    t.goto(0,0) 
+    t.end_fill()
+    tina.pencolor("#fbb400")
+    tina.begin_fill()
+    tina.fillcolor("#fbb400")
+    tina.end_fill()
+    
 def make_a_shape(t):
     """Make a shape with turtle t. Make it go left or right or forward"""    
-    ...
+    t.pencolor("#C50303")
+    t.begin_fill()
+    for triangle in range(3): 
+        tina.forward(100)
+        tina.right(30) 
+    t.goto(0,0) 
+    t.end_fill()
+    tina.pencolor("#fbb400")
+    tina.begin_fill()
+    tina.fillcolor("#fbb400")
+    tina.end_fill()
 
-# 2) Call make_a_shape() in a loop to make the turtle draw a spiral.
+
+# 2: Call make_a_shape() in a loop to make the turtle draw a spiral.
 # For instance, you can call make_a_shape() 100 times to make a spiral with 100 shapes.
 # The second ... in the for loop should be the number of shapes you want to make,
 # for example 100, or a list of numbers.
 
-num_shapes = ...
+num_shapes = 16
 
-for i in range(...):
-    make_a_shape(t)
-    t.right(360/num_shapes)
+for i in range(num_shapes):
+    if i%4 == 0:
+        tina.fillcolor("#db2a27")
+    elif i%4 == 1:
+        tina.fillcolor("#fd0905")
+    elif i%4 == 2:
+        tina.fillcolor("#fc4f4c")
+    else:
+        tina.fillcolor("#fb8684")   
+    make_a_shape(tina)
+    tina.right(360/num_shapes)
+
+turtle.done()
